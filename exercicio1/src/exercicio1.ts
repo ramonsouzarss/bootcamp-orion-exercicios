3fdf4975d447bf960d5798408a9c28a52806cad7
Ramon Souza <116771566+ramonsouzarss@users.noreply.github.com>
2023-10-10 22:27:25 -0300

    Update exercicio1.ts
    Tipei as variáveis e utilizei expressão regular no lugar do array

--- exercicio1/src/exercicio1.ts
+++ exercicio1/src/exercicio1.ts
@@ -1,23 +1,19 @@
 function contarVogais(palavra: string): number {
 
   // Declaração do array
-  const vogais = ["a", "á", "â", "à", "ã", "ä", "å",
-                  "e", "é", "ê", "è", "ẽ", "ë", 
-                  "i", "í", "î", "ì", "ĩ", "ï", 
-                  "o", "ó", "ô", "ò", "õ", "ö", 
-                  "u", "ú", "û", "ù", "ũ", "ü"];
+  const vogaisRegex: RegExp = /[aeiouáàâãåéêíóôõúü]/i;
 
   // Declaração de uma variável para contar a quantidade de vogais.
-  let quantidade = 0;
+  let quantidade: number = 0;
 
   // Transforma todas as letras em letras minúsculas (convenção).
-  const palavraMinuscula = palavra.toLowerCase();
+  const palavraMinuscula: string = palavra.toLowerCase();
 
   // Itera sobre cada caractere da palavra.
   for (let i = 0; i < palavraMinuscula.length; i++) {
 
     // Verifica se o caractere é uma vogal.
-    if (vogais.includes(palavraMinuscula[i])) {
+    if (vogaisRegex.includes(palavraMinuscula[i])) {
 
       // Incrementa a variável de quantidade.
       quantidade++;
@@ -27,10 +23,10 @@ function contarVogais(palavra: string): number {
   return quantidade;  
 }
 
-const palavra = "Nordvästersjökustartilleriflygspaningssimulatoranläggningsmaterielunderhållsuppföljningssystemdiskussionsinläggsförberedelsearbeten";
+const palavra: string = "Nordvästersjökustartilleriflygspaningssimulatoranläggningsmaterielunderhållsuppföljningssystemdiskussionsinläggsförberedelsearbeten";
 
 // Chama a função contarVogais para contar as vogais na palavraExemplo.
-const quantidadeVogais = contarVogais(palavra);
+const quantidadeVogais: number = contarVogais(palavra);
 
 // Exibe o resultado da contagem de vogais.
 console.log(`A palavra "${palavra}" contém ${quantidadeVogais} vogais.`);
